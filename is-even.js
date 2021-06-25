10093faffbaeccb0cb616f04889a1c55bafd5a5b
Pete Hamrick <pete.hamrick@gmail.com>
2021-06-24 17:13:42 -0700

    initial commit with soup for code

---
+++ is-even.js
@@ -0,0 +1,28 @@
+// function isEven(num1, num2) {
+    
+//     return num1 % num2;
+//     
+
+// }
+
+// export default isEven;
+
+export function isEven(num1, num2) {
+    return num1 % num2;
+}
+
+export function isOdd(num1, num2) {
+    // Can i do the above but then spit not zero is false?
+    // 
+}
+
+// if statement
+// 
+// if num2 > 0 {
+    // console.log('Is odd')
+// }
+// 
+// ternary?
+// function isOdd()
+    // 
+// };
--- test/tests.js
+++ test/tests.js
@@ -1,5 +1,6 @@
 // IMPORT MODULES under test here:
 // import { add } from '../calculator.js';
+import { isEven } from '../is-even.js';
 
 const test = QUnit.test;
 
@@ -8,6 +9,7 @@ test('time to test a function', (expect) => {
     //Arrange
     // Set up your arguments and expectations
     const expected = true;
+
     
     //Act 
     // Call the function you're testing and set the result to a const
@@ -17,3 +19,33 @@ test('time to test a function', (expect) => {
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
 });
+
+//test to check if a number is even
+//I want to use a remainder to test for even/odd numbers
+//I'll divide everything by 2
+//isEven(num1, num2) num2 should always be 2 since even numbers divide by 2
+test('is this even?', (expect) => {
+    //arrange: arguments and expectations
+    //
+    const expected = 0;
+
+    //Act: call the function you're testing and set the result to a const
+    const actual = isEven(5, 2);
+
+    //Expect: make assertions about what is expected vs actual result
+    expect.equal(actual, expected);
+
+});
+
+// test('is this odd?', (expect) => {
+//     //arrange: arguments and expectations
+//     //
+//     const expected > 0;
+
+//     //Act: call the function you're testing and set the result to a const
+//     const actual = isOdd(5, 2);
+
+//     //Expect: make assertions about what is expected vs actual result
+//     expect.equal(actual, expected);
+
+// });
