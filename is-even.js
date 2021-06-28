58335a601c0dee08b30606df3917c28c87d0007c
Pete Hamrick <pete.hamrick@gmail.com>
2021-06-28 09:16:02 -0700

    passing tests for even and odd

--- is-even.js
+++ is-even.js
@@ -1,28 +1,7 @@
-// function isEven(num1, num2) {
-    
-//     return num1 % num2;
-//     
-
-// }
-
-// export default isEven;
-
-export function isEven(num1, num2) {
-    return num1 % num2;
+export function isEven(number) {
+    return (number % 2) === 0;
 }
 
-export function isOdd(num1, num2) {
-    // Can i do the above but then spit not zero is false?
-    // 
+export function isOdd(number) {
+    return (number % 2) !== 0;
 }
-
-// if statement
-// 
-// if num2 > 0 {
-    // console.log('Is odd')
-// }
-// 
-// ternary?
-// function isOdd()
-    // 
-// };
--- test/tests.js
+++ test/tests.js
@@ -1,11 +1,12 @@
 // IMPORT MODULES under test here:
 // import { add } from '../calculator.js';
 import { isEven } from '../is-even.js';
+import { isOdd } from '../is-even.js';
 
 const test = QUnit.test;
 
 // name your test by what it is testing
-test('time to test a function', (expect) => {
+test('Is this even?', (expect) => {
     //Arrange
     // Set up your arguments and expectations
     const expected = true;
@@ -13,39 +14,17 @@ test('time to test a function', (expect) => {
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true; // use your function here
+    const actual = isEven(8); // use your function here
 
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
 });
 
-//test to check if a number is even
-//I want to use a remainder to test for even/odd numbers
-//I'll divide everything by 2
-//isEven(num1, num2) num2 should always be 2 since even numbers divide by 2
-test('is this even?', (expect) => {
-    //arrange: arguments and expectations
-    //
-    const expected = 0;
+test('Is this odd?', (expect) => {
+    const expected = true;
 
-    //Act: call the function you're testing and set the result to a const
-    const actual = isEven(5, 2);
+    const actual = isOdd(7);
 
-    //Expect: make assertions about what is expected vs actual result
     expect.equal(actual, expected);
-
-});
-
-// test('is this odd?', (expect) => {
-//     //arrange: arguments and expectations
-//     //
-//     const expected > 0;
-
-//     //Act: call the function you're testing and set the result to a const
-//     const actual = isOdd(5, 2);
-
-//     //Expect: make assertions about what is expected vs actual result
-//     expect.equal(actual, expected);
-
-// });
+})
