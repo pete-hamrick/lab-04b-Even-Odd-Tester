934e155f9404a9bc2053c06197738eca4cfd2f27
Pete Hamrick <pete.hamrick@gmail.com>
2021-06-28 09:55:12 -0700

    working even and odd tester

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 // import functions
-import { iseven, isOdd } from '../is-even.js';
+import { isEven, isOdd } from '../is-even.js';
 // reference needed DOM elements
 const numInput = document.getElementById('num-input');
 const evenImg = document.getElementById('even-img');
 const oddImg = document.getElementById('odd-img');
 const button = document.getElementById('number-button');
 
+function showImage(result) {
+    const value = Number(result);
+    const evenResult = isEven(value);
 
+    if (evenResult) {
+        evenImg.classList.toggle('hidden');
+        setTimeout(()=>{
+            evenImg.classList.toggle('hidden');
+        }, 3000);
+    } else {
+        oddImg.classList.toggle('hidden');
+        setTimeout(()=>{
+            oddImg.classList.toggle('hidden');
+        }, 3000);
+    }
+}
 // set event listeners 
   // get user input(s)
   // do any needed work with the value(s)
@@ -14,5 +29,6 @@ const button = document.getElementById('number-button');
 
 
 button.addEventListener('click', ()=> {
-    console.log('you clicked the button!');
+    const num = numInput.value;
+    showImage(num);
 });
